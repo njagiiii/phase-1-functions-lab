d870fd7339feb010294e4c28214cf80a18274843
njagiiii <grace.makena@student.moringaschool.com>
2023-06-13 23:44:50 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Code your solution in this file!
+
+//global variable scope
+  const numberofblocks=50;
+//global function
+function distanceFromHqInBlocks (numberofblocks){
+    if (numberofblocks < 42){
+        return (42 - numberofblocks);
+    }
+    else{
+        return(numberofblocks - 42)
+    };
+    
+}
+ //global variable
+ let Blocks = 8;
+ let feet = 2112
+ function distanceFromHqInFeet(){
+          distanceFromHqInBlocks(Blocks, feet);
+          if (blocks > headquarter){
+            return ((blocks -headquarter)*264);
+        }
+        else {
+            return ((headquarter-blocks)*264);
+        }
+       }
+ 
+ 
+//global variable
+  let blocks = 50;
+  let headquarter= 42;
+ 
+   function distanceTravelledInFeet(blocks,headquarter){
+    if (blocks > headquarter){
+        return ((blocks -headquarter)*264);
+    }
+    else {
+        return ((headquarter-blocks)*264);
+    }
+   }
+     
+function calculatesFarePrice(start, destination){
+    let distance=distanceTravelledInFeet(start,destination);
+    if (distance<=400){
+        return 0;
+    }
+    if (distance > 400 && distance<= 2000){
+        return 2.56;
+    }
+    else if(distance> 2000 && distance<= 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far'
+    };
+
+}
+
+
+
+
